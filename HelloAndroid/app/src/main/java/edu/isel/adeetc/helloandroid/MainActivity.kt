54d921721b315a920fe4b8e71bfc5df78814a2cb
Paulo Pereira <palbp.giga@gmail.com>
2019-09-16 16:17:08 +0100

    Adiciona a primeira demo usando Kotlin no desenvolvimento de aplicações Android

---
+++ HelloAndroid/app/src/main/java/edu/isel/adeetc/helloandroid/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.isel.adeetc.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
