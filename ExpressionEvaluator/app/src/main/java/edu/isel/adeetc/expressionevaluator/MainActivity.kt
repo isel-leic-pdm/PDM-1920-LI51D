0de20087cba923ea1d5fc46abf000fa17e92be1c
Paulo Pereira <palbp.giga@gmail.com>
2019-09-20 15:14:40 +0100

    Acrescenta suporte para preservação de estado de apresentação ao ExpressionEvaluator

--- ExpressionEvaluator/app/src/main/java/edu/isel/adeetc/expressionevaluator/MainActivity.kt
+++ ExpressionEvaluator/app/src/main/java/edu/isel/adeetc/expressionevaluator/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.isel.adeetc.expressionevaluator
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
+const val VIEWSTATE_KEY = "expression"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            result.text = savedInstanceState.getString(VIEWSTATE_KEY)
+        }
+
         compute.setOnClickListener {
             val ast = parse(input.text.toString())
             result.text = "${prettyPrint(ast)} = ${evaluate(ast)}"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VIEWSTATE_KEY, result.text.toString())
     }
 }
